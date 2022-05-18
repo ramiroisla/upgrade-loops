5880211f73c1f33d23b3b7221761c79ebea94db4
Ramiro Alfonso Isla Villanueva <100206000+ramiroisla@users.noreply.github.com>
2022-05-18 20:53:45 +0200

    Iteration3

---
+++ Iteration3.js
@@ -0,0 +1,15 @@
+// Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log***
+// sus valores.
+
+const placesToTravel = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+
+for (const iterator of placesToTravel) {
+  console.log(placesToTravel);
+}
